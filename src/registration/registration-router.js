200ee0b61e56c4472fc9738af5349837c9200cc3
Harvey Le <harveyxcle@gmail.com>
2020-07-27 18:39:35 -0400

    updated registration-router

--- src/registration/registration-router.js
+++ src/registration/registration-router.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
-const foldersRouter = express.Router();
+const registrationRouter = express.Router();
+const RegistrationService = require('./registration-service');
 const jsonParser = express.json();
 
 registrationRouter
   .route('/:stateAbv')
-  .get((req, res, next) => {
-    
+  .all((req, res, next) => {
+    RegistrationService.getByState(req.app.get('db'), req.params.stateAbv)
+      .then((stateAbv) => {
+        if (!stateAbv) {
+          return res.status(404).json({
+            error: { message: `State doesn't exist` },
+          });
+        }
+        res.stateAbv = stateAbv;
+        next();
+      })
+      .catch(next);
   })
+
+  module.exports = registrationRouter;
